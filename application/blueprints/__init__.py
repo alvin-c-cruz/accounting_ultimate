d5c3cac4e3fa3b012c16849830221a2512e58b66
alvin-c-cruz <alvinccruz12@gmail.com>
2025-09-08 12:17:50 +0800

    Added /measure

--- application/blueprints/__init__.py
+++ application/blueprints/__init__.py
@@ -4,6 +4,7 @@ from . import user
 from . import account
 from .register import vendor
 from .register import customer
+from .register import measure
 
 from .books_of_accounts import sales
 from .books_of_accounts import receipt
